940096ab98dfd7e8f8930ed83b9f13781873407d
kolyunyauser015 <89058507+kolyunyauser015@users.noreply.github.com>
2024-08-12 12:04:51 +0600

    Add files via upload

---
+++ module_2_hard.py
@@ -0,0 +1,17 @@
+n = int(input('Ввведите число из первого поля '))
+dividers_list = []
+divider = 2
+while divider <= n:
+    if n % divider == 0:
+        dividers_list.append(divider)
+        divider += 1
+    else:
+        divider += 1
+result = []
+for i in range(1, int(dividers_list[-1])//2+1):
+    for j in dividers_list:
+        if i < j-i:
+            result.append(i)
+            result.append(j - i)
+print('Ваш пароль:')
+print(*result)
